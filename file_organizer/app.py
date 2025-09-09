a742b49d83e5bce193d6d24ae4ee45b29b551cdc
Richard Lin <richard9774lin@gmail.com>
2025-09-09 16:24:33 -0400

    added feature where it prompts the user the target directory path

--- file_organizer/app.py
+++ file_organizer/app.py
@@ -1,7 +1,17 @@
 import os
+import shutil
 
-print(os.getcwd())
+def organize_files(directory):
+    for filename in os.listdir(directory):
+        if os.path.isfile(os.path.join(directory, filename)):
+            ext = filename.split('.')[-1]
+            folder = os.path.join(directory, ext.upper() + '_Files')
+            os.makedirs(folder, exist_ok=True)
+            shutil.move(os.path.join(directory, filename), os.path.join(folder, filename))
 
-os.chdir("/code/python/sandbox_python")
-
-print(os.getcwd())
+if __name__ == "__main__":
+    print("Please input the target directory path where files need to be organized: ")
+    path = input().strip()
+    target_dir = (path)  # Change to your target directory
+    organize_files(target_dir)
+    print(f"Files in '{target_dir}' have been organized by file type.")
